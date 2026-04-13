9c47358fe6559ed10ba19b8771ae64cb1c471a51
thanhtruc <thanhtruc779714@gmail.com>
2026-04-13 23:15:49 +0700

    deploy docker web

--- config/database.php
+++ config/database.php
@@ -1,10 +1,10 @@
 <?php
-$servername = "db";
+$servername = "containers-xxx.railway.app";
 $username = "root";
-$password = "root";
-$dbname = "truyentranh";
+$password = "PASSWORD_MOI";
+$dbname = "railway";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, 3306);
 
 if ($conn->connect_error) {
     die("Kết nối thất bại: " . $conn->connect_error);
